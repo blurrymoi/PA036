fd1472db18ac28d4c5a91849f96a2d227ec8d79c
blurrymoi <zuzan.baran@gmail.com>
2017-04-10 09:12:23 +0200

    Add service-level logging for remaining service classes

--- logging-service/src/main/java/cz/muni/pa036/logging/service/EventServiceImpl.java
+++ logging-service/src/main/java/cz/muni/pa036/logging/service/EventServiceImpl.java
@@ -7,6 +7,7 @@ import cz.muni.pa036.logging.entity.Event;
 import cz.muni.pa036.logging.entity.Invitation;
 import cz.muni.pa036.logging.entity.Sport;
 import cz.muni.pa036.logging.entity.Sportsman;
+import cz.muni.pa036.logging.helper.CRUDLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,8 @@ import java.util.Set;
  */
 @Service
 public class EventServiceImpl implements EventService {
+
+    private final CRUDLogger CRUD_LOGGER = new CRUDLogger(this.getClass());
 
     @Autowired
     private EventDAO eventDAO;
@@ -36,54 +39,63 @@ public class EventServiceImpl implements EventService {
     @Override
     public void create(Event event) {
 
+        CRUD_LOGGER.logCreate(event);
         eventDAO.create(event);
     }
 
     @Override
     public Event findById(Long id) {
 
+        CRUD_LOGGER.logFindBy("ID", id);
         return eventDAO.findById(id);
     }
 
     @Override
     public List<Event> findByName(String name) {
 
+        CRUD_LOGGER.logFindBy("name", name);
         return eventDAO.findByName(name);
     }
 
     @Override
     public List<Event> findByDate(Calendar date) {
 
+        CRUD_LOGGER.logFindBy("date", date);
         return eventDAO.findByDate(date);
     }
 
     @Override
     public List<Event> findBySport(Sport sport) {
 
+        CRUD_LOGGER.logFindBy("sport", sport);
         return eventDAO.findBySport(sport);
     }
 
     @Override
     public List<Event> findByCity(String city) {
 
+        CRUD_LOGGER.logFindBy("city", city);
         return eventDAO.findByCity(city);
     }
 
     @Override
     public List<Event> findByAdmin(Sportsman admin) {
 
+        CRUD_LOGGER.logFindBy("admin", admin);
         return eventDAO.findByAdmin(admin);
     }
 
     @Override
     public List<Event> findByParticipant(Sportsman participant) {
 
+        CRUD_LOGGER.logFindBy("participant", participant);
         return eventDAO.findByParticipant(participant);
     }
 
     @Override
     public List<Event> findAll() {
 
+        CRUD_LOGGER.logFindAll();
         return eventDAO.findAll();
     }
 
@@ -91,6 +103,7 @@ public class EventServiceImpl implements EventService {
     public void update(Event event) {
         Set<Sportsman> participants = new HashSet<>();
         participants.addAll(event.getParticipants());
+        CRUD_LOGGER.logUpdate(event);
         eventDAO.update(event);
         notificationService.notifyEventEdited(participants, event);
     }
@@ -110,16 +123,23 @@ public class EventServiceImpl implements EventService {
             notificationService.notifyEventCanceled(participants, event);
 
             //next delete results due to foreign constraints
-            resultDAO.findByEvent(event).forEach(result -> resultDAO.delete(result));
+            CRUD_LOGGER.logFindBy("event", event);
+            resultDAO.findByEvent(event).forEach(result -> {
+                CRUD_LOGGER.logDelete(result);
+                resultDAO.delete(result);
+            });
         }
 
         //next delete all invitations for this event due to foreign key
+        CRUD_LOGGER.logFindBy("event", event);
         List<Invitation> invitationList = invitationDAO.findByEvent(event);
         if (invitationList != null) {
-            for(Invitation invitation : invitationDAO.findByEvent(event)) {
+            for(Invitation invitation : invitationList) {
+                CRUD_LOGGER.logDelete(invitation);
                 invitationDAO.delete(invitation);
             }
         }
+        CRUD_LOGGER.logDelete(event);
         eventDAO.delete(event);
     }
 
@@ -127,7 +147,7 @@ public class EventServiceImpl implements EventService {
     public void edit(Event event) {
         Set<Sportsman> participants = new HashSet<>();
         participants.addAll(event.getParticipants());
-
+        CRUD_LOGGER.logUpdate(event);
         eventDAO.update(event);
         notificationService.notifyEventEdited(participants, event);
     }
--- logging-service/src/main/java/cz/muni/pa036/logging/service/SportsmanServiceImpl.java
+++ logging-service/src/main/java/cz/muni/pa036/logging/service/SportsmanServiceImpl.java
@@ -3,6 +3,7 @@ package cz.muni.pa036.logging.service;
 import cz.muni.pa036.logging.dao.SportsmanDAO;
 import cz.muni.pa036.logging.entity.Sportsman;
 import cz.muni.pa036.logging.utils.Constants;
+import cz.muni.pa036.logging.helper.CRUDLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,6 +17,8 @@ import java.util.List;
 @Service
 public class SportsmanServiceImpl implements SportsmanService {
 
+	private final CRUDLogger CRUD_LOGGER = new CRUDLogger(this.getClass());
+
 	@Autowired
 	private SportsmanDAO sportsmanDAO;
 
@@ -27,53 +30,63 @@ public class SportsmanServiceImpl implements SportsmanService {
 		}
 
 		sportsman.setPassword(hashPassword(sportsman.getPassword()));
+		CRUD_LOGGER.logCreate(sportsman);
 		sportsmanDAO.create(sportsman);
 	}
 
 	@Override
 	public Sportsman findById(Long id) {
 
+		CRUD_LOGGER.logFindBy("ID", id);
 		return sportsmanDAO.findById(id);
 	}
 
 	@Override
 	public List<Sportsman> findByName(String name) {
 
+		CRUD_LOGGER.logFindBy("name", name);
 		return sportsmanDAO.findByName(name);
 	}
 
 	@Override
 	public List<Sportsman> findBySurname(String surname) {
 
+		CRUD_LOGGER.logFindBy("surname", surname);
 		return sportsmanDAO.findBySurname(surname);
 	}
 
 	@Override
 	public Sportsman findByEmail(String email) {
 
+		CRUD_LOGGER.logFindBy("email", email);
 		return sportsmanDAO.findByEmail(email);
 	}
 
 	@Override
 	public List<Sportsman> findAll() {
 
+		CRUD_LOGGER.logFindAll();
 		return sportsmanDAO.findAll();
 	}
 
 	@Override
 	public void update(Sportsman sportsman) {
 
+		CRUD_LOGGER.logUpdate(sportsman);
 		sportsmanDAO.update(sportsman);
 	}
 
 	@Override
 	public List<Sportsman> findBySubstring(String substring, Long event_id) {
+
+		CRUD_LOGGER.logFindBy("substring", substring);
 		return sportsmanDAO.findBySubstring(substring, event_id);
 	}
 
 	@Override
 	public void delete(Sportsman sportsman) {
 
+		CRUD_LOGGER.logDelete(sportsman);
 		sportsmanDAO.delete(sportsman);
 	}
 
